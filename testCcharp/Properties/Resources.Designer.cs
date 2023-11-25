122c24a5bd68450450f18e90190c01b3f6feaa96
majda-dev <ma.alaabouch23@gmail.com>
2023-11-25 12:49:37 +0100

    fix error

--- testCcharp/Properties/Resources.Designer.cs
+++ testCcharp/Properties/Resources.Designer.cs
@@ -83,16 +83,6 @@ namespace testCcharp.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap logo_me {
-            get {
-                object obj = ResourceManager.GetObject("logo-me", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap Majda {
             get {
                 object obj = ResourceManager.GetObject("Majda", resourceCulture);
